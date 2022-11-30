60f472a5d465b877f9d1b0630a45f205eb687f0a
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-11-29 22:54:58 -0300

    Date and hours

--- clock.js
+++ clock.js
@@ -2,10 +2,11 @@ setInterval(() => {
   let date = new Date()
   let clock = document.getElementById('clock');
 
-  clock.innerHTML =
-  date.getHours()+':'+
-  date.getMinutes()+':'+
-  date.getSeconds();
+  clock.innerHTML = `
+  ${date.getDate()}/${(date.getMonth()+ 1)}/${date.getFullYear()}       
+  
+  ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} 
+  `;
 
 
 }, 1000);
